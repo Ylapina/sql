c95874a21a035bc8a1f5d5daf1e81296d888a4b3
Yelena <ylapina11@gmail.com>
2018-10-27 12:11:34 -0700

    initial commit

---
+++ SQL sample sequal scripts.sql
@@ -0,0 +1,12 @@
+select * from product;
+Select * from product where ID =2;
+insert into Product (code, Description, Listprice)
+values ( 'web', 'Web development', 54.55);
+select * from Product where listprice < 55;
+select * from Product where code = 'web';
+Insert into product ( code, description, listprice)
+values ( 'teach',  'for teachers only', 67.50);
+insert into product (code, description, listprice)
+values ( 'dummies', 'for beginners', '45.34');
+
+
